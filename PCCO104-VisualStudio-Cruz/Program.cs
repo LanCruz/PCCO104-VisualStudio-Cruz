46dd07b67dc2fe245e395b4c7e6fd5659ed6f1bc
user <user@DESKTOP-LPFB2EN>
2024-03-16 11:11:31 +0800

    Add project files.

---
+++ PCCO104-VisualStudio-Cruz/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PCCO104_VisualStudio_Cruz
+{
+    internal class Program{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            string[] names = {"YEYEYE", "NONONO"};
+            foreach(var name in names)
+            {
+                Console.WriteLine($"{name}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
